d3e1daa8d886aafbf4fe37a465e13cf978a80f46
v0o0v <v0o0v@daum.net>
2020-03-30 15:14:54 +0900

    테스트용 controller 생성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @GetMapping("/test1")
+    public String test1() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+        return "test1";
     }
 
 }
